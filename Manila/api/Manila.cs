b38f1f3ee8d494d8c0c1c844365c5fb445db5347
Justin Jakob <justin.jakob391@outlook.com>
2025-03-22 18:59:31 +0100

    Added brief doc to the main api class

--- Manila/api/Manila.cs
+++ Manila/api/Manila.cs
@@ -9,66 +9,135 @@ namespace Shiron.Manila.API;
 /// The main Manila API class. Used for global functions.
 /// </summary>
 public sealed class Manila {
-    private ScriptContext context;
-    private BuildConfig buildConfig = new BuildConfig();
+	private ScriptContext context;
+	private BuildConfig buildConfig = new BuildConfig();
 
+	public Manila(ScriptContext context) {
+		this.context = context;
+	}
 
-    public Manila(ScriptContext context) {
-        this.context = context;
-    }
+	/// <summary>
+	/// Gets the current project in the Manila engine.
+	/// </summary>
+	/// <returns>The current project.</returns>
+	/// <exception cref="Exception">Thrown when not in a project context.</exception>
+	public Project getProject() {
+		if (ManilaEngine.getInstance().currentProject == null) throw new Exception("Not in project context.");
+		return ManilaEngine.getInstance().currentProject;
+	}
 
-    public Project getProject() {
-        if (ManilaEngine.getInstance().currentProject == null) throw new Exception("Not in project context.");
-        return ManilaEngine.getInstance().currentProject;
-    }
-    public UnresolvedProject getProject(string name) {
-        return new UnresolvedProject(name);
-    }
+	/// <summary>
+	/// Gets an unresolved project with the specified name.
+	/// </summary>
+	/// <param name="name">The name of the project to get.</param>
+	/// <returns>An unresolved project with the specified name.</returns>
+	public UnresolvedProject getProject(string name) {
+		return new UnresolvedProject(name);
+	}
 
-    public Workspace getWorkspace() {
-        return ManilaEngine.getInstance().workspace;
-    }
+	/// <summary>
+	/// Gets the workspace in the Manila engine.
+	/// </summary>
+	/// <returns>The workspace in the Manila engine.</returns>
+	public Workspace getWorkspace() {
+		return ManilaEngine.getInstance().workspace;
+	}
 
-    public BuildConfig getConfig() {
-        return buildConfig;
-    }
+	/// <summary>
+	/// Gets the build configuration for this Manila instance.
+	/// </summary>
+	/// <returns>The build configuration for this Manila instance.</returns>
+	public BuildConfig getConfig() {
+		return buildConfig;
+	}
 
-    public SourceSet sourceSet(string origin) {
-        return new SourceSet(origin);
-    }
-    public Task task(string name) {
-        return new Task(name, ManilaEngine.getInstance().currentProject, context);
-    }
-    public Dir dir(string path) {
-        return new Dir(path);
-    }
-    public File file(string path) {
-        return new File(path);
-    }
+	/// <summary>
+	/// Creates a new source set with the specified origin.
+	/// </summary>
+	/// <param name="origin">The origin of the source set.</param>
+	/// <returns>A new source set with the specified origin.</returns>
+	public SourceSet sourceSet(string origin) {
+		return new SourceSet(origin);
+	}
 
-    public void import(string pluginKey) {
-        var plugin = ExtensionManager.getInstance().getPlugin(pluginKey);
-        import(plugin);
-    }
-    public void import(ScriptObject obj) {
-        var plugin = ExtensionManager.getInstance().getPlugin((string) obj["group"], (string) obj["name"], (string) obj["version"]);
-        import(plugin);
-    }
-    public void import(ManilaPlugin plugin) {
-        Logger.debug("Importing: " + plugin);
-    }
+	/// <summary>
+	/// Creates a new task with the specified name.
+	/// </summary>
+	/// <param name="name">The name of the task to create.</param>
+	/// <returns>A new task with the specified name, associated with the current project and script context.</returns>
+	public Task task(string name) {
+		return new Task(name, ManilaEngine.getInstance().currentProject, context);
+	}
 
-    public void apply(string pluginComponentKey) {
-        var component = ExtensionManager.getInstance().getPluginComponent(pluginComponentKey);
-        apply(component);
-    }
-    public void apply(ScriptObject obj) {
-        var version = obj.GetProperty("version");
-        var component = ExtensionManager.getInstance().getPluginComponent((string) obj["group"], (string) obj["name"], (string) obj["component"], version == Undefined.Value ? null : (string) version);
-        apply(component);
-    }
-    public void apply(PluginComponent component) {
-        Logger.debug("Applying: " + component);
-        getProject().applyComponent(component);
-    }
+	/// <summary>
+	/// Creates a new directory reference with the specified path.
+	/// </summary>
+	/// <param name="path">The path of the directory.</param>
+	/// <returns>A new directory reference with the specified path.</returns>
+	public Dir dir(string path) {
+		return new Dir(path);
+	}
+
+	/// <summary>
+	/// Creates a new file reference with the specified path.
+	/// </summary>
+	/// <param name="path">The path of the file.</param>
+	/// <returns>A new file reference with the specified path.</returns>
+	public File file(string path) {
+		return new File(path);
+	}
+
+	/// <summary>
+	/// Imports the plugin with the specified key.
+	/// </summary>
+	/// <param name="pluginKey">The key of the plugin to import.</param>
+	public void import(string pluginKey) {
+		var plugin = ExtensionManager.getInstance().getPlugin(pluginKey);
+		import(plugin);
+	}
+
+	/// <summary>
+	/// Imports the plugin specified by the script object.
+	/// </summary>
+	/// <param name="obj">A script object containing the group, name, and version of the plugin to import.</param>
+	public void import(ScriptObject obj) {
+		var plugin = ExtensionManager.getInstance().getPlugin((string) obj["group"], (string) obj["name"], (string) obj["version"]);
+		import(plugin);
+	}
+
+	/// <summary>
+	/// Imports the specified Manila plugin.
+	/// </summary>
+	/// <param name="plugin">The Manila plugin to import.</param>
+	public void import(ManilaPlugin plugin) {
+		Logger.debug("Importing: " + plugin);
+	}
+
+	/// <summary>
+	/// Applies the plugin component with the specified key to the current project.
+	/// </summary>
+	/// <param name="pluginComponentKey">The key of the plugin component to apply.</param>
+	public void apply(string pluginComponentKey) {
+		var component = ExtensionManager.getInstance().getPluginComponent(pluginComponentKey);
+		apply(component);
+	}
+
+	/// <summary>
+	/// Applies the plugin component specified by the script object to the current project.
+	/// </summary>
+	/// <param name="obj">A script object containing the group, name, component, and optional version of the plugin component to apply.</param>
+	public void apply(ScriptObject obj) {
+		var version = obj.GetProperty("version");
+		var component = ExtensionManager.getInstance().getPluginComponent((string) obj["group"], (string) obj["name"], (string) obj["component"], version == Undefined.Value ? null : (string) version);
+		apply(component);
+	}
+
+	/// <summary>
+	/// Applies the specified plugin component to the current project.
+	/// </summary>
+	/// <param name="component">The plugin component to apply to the current project.</param>
+	public void apply(PluginComponent component) {
+		Logger.debug("Applying: " + component);
+		getProject().applyComponent(component);
+	}
 }
