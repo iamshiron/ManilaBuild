b6cae24eb9d8a89e50631d36df1a3efb8be06c3b
Justin Jakob <justin.jakob391@outlook.com>
2025-07-11 23:56:07 +0200

    Added ConfigurationCache base class

---
+++ Manila/caching/ConfigurationCache.cs
@@ -0,0 +1,16 @@
+
+using Shiron.Manila.API;
+
+namespace Shiron.Manila.Caching;
+
+public class ConfigurationCache {
+    public void Cache(Project project) { }
+
+    public Project Load() {
+        throw new NotImplementedException();
+    }
+
+    public bool Valid(Project project) {
+        throw new NotImplementedException();
+    }
+}
