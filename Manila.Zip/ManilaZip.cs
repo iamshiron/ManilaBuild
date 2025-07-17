d2946c407f7cda679d9b218ebe423218962c04c6
Justin Jakob <justin.jakob391@outlook.com>
2025-07-17 18:38:56 +0200

    Added zip plugin for testing artifacts

---
+++ Manila.Zip/ManilaZip.cs
@@ -0,0 +1,20 @@
+﻿using Shiron.Manila.Attributes;
+using Shiron.Manila.Ext;
+
+namespace Manila.Zip;
+
+public class ManilaZip : ManilaPlugin {
+    public ManilaZip() : base("shiron.manila", "zip", "1.0.0", ["Shiron"], []) {
+    }
+
+    [PluginInstance]
+    public static ManilaZip? Instance { get; private set; }
+
+    public override void Init() {
+        Debug("Init");
+    }
+
+    public override void Release() {
+        Debug("Release");
+    }
+}
