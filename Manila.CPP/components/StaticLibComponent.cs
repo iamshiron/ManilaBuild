105cf14cad24ecd48d67a798fe7c325a19938280
Justin Jakob <justin.jakob391@outlook.com>
2025-07-24 03:32:59 +0200

    Removed C++ plugin to simplify the architecture for now. The Zip plugin will be enough for testing

--- Manila.CPP/components/StaticLibComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Shiron.Manila.CPP.Components;
-
-/// <summary>
-/// Represents a C++ static library project.
-/// </summary>
-public class StaticLibComponent : CppComponent {
-    public StaticLibComponent() : base("staticlib") {
-    }
-}
