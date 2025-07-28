dd4b6de6ee02e3102c30ec4c090c0506ea9e5bae
Justin Jakob <justin.jakob391@outlook.com>
2025-07-28 06:13:31 +0200

    Improved API by moving dependencies to artifacts

--- Manila/api/Component.cs
+++ Manila/api/Component.cs
@@ -23,24 +23,9 @@ public class Component(ILogger logger, string rootDir, string path) {
     public DirHandle Path { get; private set; } = new DirHandle(path);
 
     /// <summary>
-    /// Collection of plugin components applied to this component.
-    /// </summary>
-    public Dictionary<Type, PluginComponent> PluginComponents { get; } = [];
-
-    /// <summary>
-    /// List of plugin types applied to this component.
-    /// </summary>
-    public List<Type> Plugins { get; } = [];
-
-    /// <summary>
     /// Collection of jobs belonging to this component.
     /// </summary>
     public List<Job> Jobs { get; } = [];
-
-    /// <summary>
-    /// Types of dependencies used by this component.
-    /// </summary>
-    public List<Type> DependencyTypes { get; } = [];
 
     /// <summary>
     /// Returns a unique identifier for this component.
@@ -49,44 +34,6 @@ public class Component(ILogger logger, string rootDir, string path) {
     public virtual string GetIdentifier() {
         string relativeDir = System.IO.Path.GetRelativePath(RootDir, Path.Handle);
         return relativeDir.Replace(System.IO.Path.DirectorySeparatorChar, ':').ToLower();
-    }
-
-    /// <summary>
-    /// Checks if a component type is applied to this component.
-    /// </summary>
-    /// <typeparam name="T">The component type to check.</typeparam>
-    /// <returns>True if the component is applied.</returns>
-    public bool HasComponent<T>() where T : PluginComponent {
-        return PluginComponents.ContainsKey(typeof(T));
-    }
-    /// <summary>
-    /// Gets a component instance of the specified type.
-    /// </summary>
-    /// <typeparam name="T">The component type to retrieve.</typeparam>
-    /// <returns>The component instance.</returns>
-    /// <exception cref="Exception">Thrown when the component is not found.</exception>
-    public T GetComponent<T>() where T : PluginComponent {
-        if (PluginComponents.TryGetValue(typeof(T), out var component))
-            return (T) component;
-
-        foreach (var p in PluginComponents) {
-            if (typeof(T).IsAssignableFrom(p.Key))
-                return (T) p.Value;
-        }
-
-        throw new ManilaException($"Component of type {typeof(T).Name} not found in this context.");
-    }
-
-    /// <summary>
-    /// Gets the language component applied to this component.
-    /// </summary>
-    /// <returns>The language component instance.</returns>
-    /// <exception cref="Exception">Thrown when no language component is found.</exception>
-    public LanguageComponent GetLanguageComponent() {
-        foreach (var component in PluginComponents.Values) {
-            if (component is LanguageComponent languageComponent) return languageComponent;
-        }
-        throw new ManilaException("No language component found.");
     }
 
     /// <summary>
--- Manila/artifacts/Artifact.cs
+++ Manila/artifacts/Artifact.cs
@@ -4,7 +4,6 @@ using Shiron.Manila.API;
 using Shiron.Manila.API.Builders;
 using Shiron.Manila.Caching;
 using Shiron.Manila.Exceptions;
-using Shiron.Manila.Logging;
 using Shiron.Manila.Utils;
 
 namespace Shiron.Manila.Artifacts;
@@ -14,7 +13,8 @@ public class Artifact(Workspace workspace, ArtifactBuilder builder) {
     public readonly Job[] Jobs = [.. builder.JobBuilders.Select(b => b.Build())];
     public readonly string Name = builder.Name ?? throw new ManilaException($"Artifact must have a name! {builder.Description}");
     public readonly UnresolvedProject Project = new UnresolvedProject(workspace, builder.Project.Name);
-    public readonly RegexUtils.PluginComponentMatch PluginComponent = builder.PluginComponent ?? throw new ManilaException($"Artifact must have a plugin component match!");
+    public readonly RegexUtils.PluginComponentMatch PluginComponent =
+        builder.PluginComponent ?? throw new ManilaException($"Artifact must have a plugin component! {builder.Description}");
 
     public LogCache? LogCache { get; internal set; } = null;
 
